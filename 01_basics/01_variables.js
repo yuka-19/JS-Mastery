b4e307955d8a49001074d527724364ae890b88d7
YUKA <140295326+yuka-19@users.noreply.github.com>
2025-09-06 01:57:44 +0000

    date and time

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 34325321
+let accountEmail = "Yuka19@example.com"
+var accountPassword = "secretpassword"
+accountCity = "Delhi"
+let accountState;
+
+//accountId = 23112 //not allowed
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+/*
+    Prefer not to use var 
+    because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
