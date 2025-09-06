f2de953a329edbfb5ad147e709df94ad0a4eb9aa
YUKA <140295326+yuka-19@users.noreply.github.com>
2025-09-06 01:59:42 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -15,5 +15,5 @@ let accountState;
     Prefer not to use var 
     because of issue in block scope and functional scope
 */
-
+ 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
