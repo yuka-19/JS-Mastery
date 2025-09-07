d268a41c0fd8adb4813ffe3011ca2f26e18452b7
YUKA <140295326+yuka-19@users.noreply.github.com>
2025-09-07 03:50:53 +0000

    operation discussed

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+// let score = "45";
+// let score = "45abc";
+// let score = true;
+// let score = false;
+let score = null;
+
+console.log(typeof score);
+
+let valueinNumber = Number (score);
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+
+
+// "45" => 45
+// "45abc" => NaN 
+// true => 1 / false => 0 
+
