ed9c895f26ac251927ee63c4feb9f82dc9370046
YUKA <140295326+yuka-19@users.noreply.github.com>
2025-09-07 02:41:45 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,30 @@
-console.log("Hello JavaScript!");
+// JavaScript Datatypes
+
+
+"use strict" ; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using Nodejs , not browser
+
+console.log(3 + 3);
+
+console.log("Yuka!");
+
+let name = "Yuka";      
+let age = 18;           
+let isLoggedIn = true;  
+
+
+//number => 2 to power 53
+//string => "Yuka"
+//bigint 
+//boolean => true/false
+//null    => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+
