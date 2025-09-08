22ed24c5beddaffd598d26c98016b7b5e2cc6afb
YUKA <140295326+yuka-19@users.noreply.github.com>
2025-09-08 16:32:23 +0000

    operation discussed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,3 +33,34 @@ let someNumber = 21
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber)
+
+
+// *********************** Operations ***********************
+
+let value = 6
+let negValue = -value
+
+// console.log(negValue)
+
+
+let str1 = "Hello"
+let str2 = " Yuka"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("2" + 1);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = "100"
+gameCounter++;
+console.log(gameCounter);
