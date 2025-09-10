33d6c6650a2854591f6c43afe5e41877443659c5
YUKA <140295326+yuka-19@users.noreply.github.com>
2025-09-10 23:47:24 +0000

    num&math

---
+++ 01_basics/06_num_&_math.js
@@ -0,0 +1,16 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(1));
+
+
+const otherNum = 53.34
+console.log(otherNum.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
