7fb5b4495337de7e93dd4d3161b5eea82ff1ab9b
YUKA <140295326+yuka-19@users.noreply.github.com>
2025-09-14 20:44:40 +0000

    num&math

--- 01_basics/06_num_&_math.js
+++ 01_basics/06_num_&_math.js
@@ -6,7 +6,7 @@ const balance = new Number(100)
 console.log(balance);
 
 console.log(balance.toString());
-console.log(balance.toFixed(1));
+console.log(balance.toFixed(2));
 
 
 const otherNum = 53.34
